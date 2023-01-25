283f67f3dcd8d7a9b00da21b61fffc3fc2815716
Владислав <95309036+Kwikli@users.noreply.github.com>
2023-01-25 18:41:36 +0200

    day7

--- src/components/App.vue
+++ src/components/App.vue
@@ -1,112 +1,151 @@
 <template>
-   <div>
-        <input type="text" name="search" v-model="search" placeholder="Пошук..."><br>
-		<table class="table table-dark">
-			<tr v-for="item in students"  v-bind:key="item.Ref">
-                <template v-if="item._id != editSt"> 
-                    <td>{{item.name}}</td>
-                    <td><input type="checkbox" :class="item.isDonePr" v-model="item.isDonePr"></td>
-                    <td>{{item.group}}</td>
-                    <td>
-                        <up @click="deleteStudent(item._id)"><img src="del.png" alt="delete"></up>
-                        <up @click="updateStudent(item._id)"><img src="edit.png" alt="delete"></up>
-                    </td>
-                </template>
-                <template v-else>
-                    <td><input type="text" v-model="item.name"></td>
-                    <td><input type="checkbox" :class="item.isDonePr" v-model="item.isDonePr"></td>
-                    <td>
-                        <select v-model="student.group">
-                            <option value="" selected>{{item.group}}</option>
-                            <option v-if="item.group != 'RPZ 19 1/9'" value="RPZ 19 1/9">RPZ 19 1/9</option>
-                            <option v-if="item.group != 'RPZ 19 2/9'" value="RPZ 19 2/9">RPZ 19 2/9</option>
-                        </select>
-                    </td>
-                     <td>
-                        <up @click="deleteStudent(item._id)"><img src="del.png" alt="delete"></up>
-                        <up @click="updateStudentPut(item._id)"><img src="edit.png" alt="delete"></up>
-                    </td>
-                </template>
-			</tr>
-		</table>
-        
-		<input v-model="student.name" placeholder="ПІБ"><br>
-		<b style="margin-left: 135px;">ПР</b> <input type="checkbox" v-model="student.isDonePr"><br>
-		<select v-model="student.group">
-            <option value="" selected disabled hidden>Виберіть групу</option>
-			<option value="RPZ 19 1/9">RPZ 19 1/9</option>
-			<option value="RPZ 19 2/9">RPZ 19 2/9</option>
-		</select>
-		<br><button @click="addStudent()">Додати студента</button>
-   </div>
+    <div class="main">
+        <h1>Mini Shop</h1>
+        <div class="content">
+             <router-view></router-view> 
+            Hello!
+        </div>
+    </div>
 </template>
- 
-<script>
-import axios from 'axios'
- 
-export default {
-    data() {
-       return {
-           students: [],
-           search:'',
-           student: {name: "", isDonePr: false, group: ""},
-           editSt:0
-       }
-    },
-    mounted() {
-        this.getData();
-        
-    },
-    methods: {
-        getData(){
-            axios.get("http://34.82.81.113:3000/students")
-                    .then(data => {
-                        this.students =  data.data;
-                        this.mounted;
-                        console.log(this.students);
-                    })
-        },
-        deleteStudent(studId) {
-            console.log('delete - ',studId);
-            axios.delete("http://34.82.81.113:3000/students/"+studId)
-                .then(data => {
-                    console.log(data); 
-                    this.getData();
-                })
-            this.$forceUpdate();
-        },
-        updateStudent(id){
-            console.log('update - ',id);
-            this.editSt = id;
-            this.$forceUpdate();
-        },
-        updateStudentPut(id){
-            let foundStudent = this.students.find((element) => {
-                return element._id == id;
-            });
 
-            axios.put("http://34.82.81.113:3000/students/"+id, {
-                name: foundStudent.name,
-                group: foundStudent.group,
-                isDonePr: foundStudent.isDonePr
-            }).then((response) => {
-                console.log(response.data);
-                this.getData();
-                this.$forceUpdate();
-            })
-            this.editSt = 0;
-            
-            
-        },
-        addStudent() {
-            console.log('add - ',this.student);
-            axios.post("http://34.82.81.113:3000/students", {...this.student})
-                .then(data => {
-                    console.log(data);
-                    this.getData();
-                })
-            this.$forceUpdate();    
-       },
-    },
-}
- </script>
+<style>
+    /* Generic styling */
+
+    h2, h3, h4, h5, h6 {
+        font-family: 'Montserrat', sans-serif;
+    }
+
+    button:focus, input:focus {
+        outline: none;
+    }
+
+    .btn {
+        background-color: #F08080;
+        border-radius: 2px;
+        color: white;
+        user-select: none;
+        border: none;
+        cursor: pointer;
+        opacity: 1;
+    }
+
+    .btn:active {
+        opacity: 0.8;
+    }
+
+    [v-cloak] {
+        display: none !important;
+    }
+
+    .fade-enter-active {
+        transition: opacity .5s
+    }
+    .fade-enter, .fade-leave-active {
+        opacity: 0
+    }
+
+    /* Sections */
+
+    html, body {
+        width: 100%;
+        height: 100%;
+        margin: 0;
+    }
+
+    body {
+        font-family: 'Lato', sans-serif;
+        background-color: #F5F5F5;
+    }
+
+    #app {
+        margin: 0 2rem;
+        padding: 0 2rem;
+        background-color: white;
+        display: flex;
+        flex-direction: column;
+    }
+
+    .header {
+        flex: 0;
+        padding: 1rem 0;
+    }
+
+    .main {
+        display: flex;
+        flex-direction: column;
+        flex: 1;
+        padding-bottom: 2rem;
+    }
+
+    .products {
+        flex: 3;
+    }
+
+    .cart {
+        flex: 2;
+    }
+
+    /* Products */
+
+    .product-list {
+        margin-right: 1rem;
+    }
+
+    .search-results {
+        padding-bottom: 1rem;
+    }
+
+    .search-results .search-term {
+        font-style: italic;
+    }
+
+    .product {
+        padding: 1rem;
+        border: 1px solid #E9E9E9;
+        border-radius: 2px;
+        overflow: auto;
+        margin: 1rem 1rem 1rem 0;
+        display: flex;
+        flex-flow: row nowrap;
+    }
+
+    .product:first-child {
+        margin-top: 0;
+    }
+
+    .product > div:first-child {
+
+    }
+
+    .product > div:last-child {
+        margin-left: 1rem;
+    }
+
+    .product-image {
+        max-height: 150px;
+        width: 250px;
+        overflow: hidden;
+        border: 1px solid #E9E9E9;
+        border-radius: 2px;
+    }
+
+    .product-image > img {
+        height: 100%;
+        display: block;
+    }
+
+    .product-title {
+        margin-top: 0;
+    }
+
+    .product .add-to-cart {
+        padding: 0.5rem 1rem;
+        font-size: 0.8rem;
+    }
+
+    #product-list-bottom {
+        text-align: center;
+        color: #AAAAAA;
+        font-size: 0.85rem;
+    }
+</style>
